89065df1d67775411da5c66b427b8a2ff0af456c
Russell Smith <russell.subscriptions@gmail.com>
2024-10-31 17:08:04 +0200

    add inline markup for text and images

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
-from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import extract_markdown_links, split_nodes_delimiter, extract_markdown_images
+from textnode import TextNode, TextType
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is a bold node", TextType.BOLD)
         split_nodes = split_nodes_delimiter([node, node], "*", TextType.BOLD)
         self.assertEqual([node, node], split_nodes)
-        
+
     def test_single_delimited(self):
         # Test multiple words
         node = TextNode("*bold text*", TextType.TEXT)
@@ -31,13 +31,13 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             [
                 TextNode("standard text ", TextType.TEXT),
                 TextNode("some bold text", TextType.BOLD),
-                TextNode( " standard ", TextType.TEXT),
+                TextNode(" standard ", TextType.TEXT),
                 TextNode("bold_text", TextType.BOLD),
                 TextNode(" more_standard", TextType.TEXT),
             ],
             split_nodes,
         )
-        
+
     def test_multiple_delimited2(self):
         node1 = TextNode(
             "standard text *some bold text* standard *bold_text* more_standard",
@@ -146,6 +146,95 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_no_match(self):
+        markdown_images = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif"
+        )  # missing close brace at end
+        self.assertListEqual([], markdown_images)
+
+    def test_single_match(self):
+        markdown_images = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        )
+        self.assertListEqual(
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif")], markdown_images
+        )
+
+    def test_dual_match(self):
+        markdown_images = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            markdown_images,
+        )
+
+    def test_boot_dev(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_no_match(self):
+        markdown_links = extract_markdown_links(
+            "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif"
+        )  # missing close brace at end
+        self.assertListEqual([], markdown_links)
+
+    def test_single_match(self):
+        markdown_links = extract_markdown_links(
+            "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        )
+        self.assertListEqual(
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif")], markdown_links
+        )
+
+    def test_dual_match(self):
+        markdown_links = extract_markdown_links(
+            "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            markdown_links,
+        )
+        
+    def test_boot_dev(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+        
+    def test_link_at_start(self):
+        matches = extract_markdown_links(
+            "[link at start](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link at start", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+        
+        
+        
 
 
 if __name__ == "__main__":
