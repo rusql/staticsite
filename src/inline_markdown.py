9e2f3586fcc3b76b28eaad7ab63602bdad671fb6
Russell Smith <russell.subscriptions@gmail.com>
2024-10-31 17:08:04 +0200

    add inline markup for text and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,5 @@
 from textnode import TextNode, TextType
-
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -21,3 +21,23 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    image_markdown_matches = re.findall("(!\[.*?\])(\(.*?\))", text)
+    images = []
+    for match in image_markdown_matches:
+        alt_text = match[0][2 : len(match[0]) - 1]
+        image_url = match[1][1 : len(match[1]) - 1]
+        images.append((alt_text, image_url))
+    return images
+
+
+def extract_markdown_links(text):
+    link_markdown_matches = re.findall("([^!]\[.*?\])(\(.*?\))", text)
+    links = []
+    for link in link_markdown_matches:
+        link_text = link[0][2 : len(link[0]) - 1]
+        link_url = link[1][1 : len(link[1]) - 1]
+        links.append((link_text, link_url))
+    return links
