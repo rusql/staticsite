14c64fe0ecfcae10424e6ae6c6fa75f6febde66a
Russell Smith <russell.subscriptions@gmail.com>
2024-12-12 15:16:40 +0200

    add split_nodes_image add split_nodes_link

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,12 @@
 import unittest
 
-from inline_markdown import extract_markdown_links, split_nodes_delimiter, extract_markdown_images
+from inline_markdown import (
+    extract_markdown_links,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    split_nodes_image,
+    split_nodes_link,
+)
 from textnode import TextNode, TextType
 
 
@@ -208,7 +214,7 @@ class TestExtractMarkdownLinks(unittest.TestCase):
             ],
             markdown_links,
         )
-        
+
     def test_boot_dev(self):
         matches = extract_markdown_links(
             "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
@@ -220,7 +226,7 @@ class TestExtractMarkdownLinks(unittest.TestCase):
             ],
             matches,
         )
-        
+
     def test_link_at_start(self):
         matches = extract_markdown_links(
             "[link at start](https://boot.dev) and [another link](https://blog.boot.dev)"
@@ -232,9 +238,77 @@ class TestExtractMarkdownLinks(unittest.TestCase):
             ],
             matches,
         )
-        
-        
-        
+
+    class TestSplitNodes(unittest.TestCase):
+        def test_split_image(self):
+            node = TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+                TextType.TEXT,
+            )
+            new_nodes = split_nodes_image([node])
+            self.assertListEqual(
+                [
+                    TextNode("This is text with an ", TextType.TEXT),
+                    TextNode(
+                        "image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"
+                    ),
+                ],
+                new_nodes,
+            )
+
+        def test_split_image_single(self):
+            node = TextNode(
+                "![image](https://www.example.COM/IMAGE.PNG)",
+                TextType.TEXT,
+            )
+            new_nodes = split_nodes_image([node])
+            self.assertListEqual(
+                [
+                    TextNode(
+                        "image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"
+                    ),
+                ],
+                new_nodes,
+            )
+
+        def test_split_images(self):
+            node = TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                TextType.TEXT,
+            )
+            new_nodes = split_nodes_image([node])
+            self.assertListEqual(
+                [
+                    TextNode("This is text with an ", TextType.TEXT),
+                    TextNode(
+                        "image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"
+                    ),
+                    TextNode(" and another ", TextType.TEXT),
+                    TextNode(
+                        "second image",
+                        TextType.IMAGE,
+                        "https://i.imgur.com/3elNhQu.png",
+                    ),
+                ],
+                new_nodes,
+            )
+
+        def test_split_links(self):
+            node = TextNode(
+                "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+                TextType.TEXT,
+            )
+            new_nodes = split_nodes_link([node])
+            self.assertListEqual(
+                [
+                    TextNode("This is text with a ", TextType.TEXT),
+                    TextNode("link", TextType.LINK, "https://boot.dev"),
+                    TextNode(" and ", TextType.TEXT),
+                    TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                    TextNode(" with text that follows", TextType.TEXT),
+                ],
+                new_nodes,
+            )
 
 
 if __name__ == "__main__":
