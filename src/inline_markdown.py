c3cc72582bdaaf0ba3c204f6924e04a1cf3b1307
Russell Smith <russell.subscriptions@gmail.com>
2024-12-16 12:35:52 +0200

    Add Text to TextNode inline functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -87,3 +87,13 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = split_nodes_delimiter([TextNode(text, TextType.TEXT)], "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
