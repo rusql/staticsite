0912aeaaae032a978948708ce51b9596f68955d1
Russell Smith <russell.subscriptions@gmail.com>
2024-12-18 10:43:17 +0200

    Add BLOCKTYPE Enum

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,18 @@
+import enum as e
+
+class BlockType(e.Enum):
+    HEADING1 = "heading 1"
+    HEADING2 = "heading 2"
+    HEADING3 = "heading 3"
+    HEADING4 = "heading 4"
+    HEADING5 = "heading 5"
+    HEADING6 = "heading 6"
+    PARAGRAPH = "paragraph"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     lines = markdown.split("\n")
     lines.append("") #add an empty line to the list otherwise we skip the last element
@@ -11,3 +26,59 @@ def markdown_to_blocks(markdown):
         else:
             this_line += line+"\n"
     return blocks
+
+def list_items_all_start_with(list, s):
+    if len(list) == 0:
+        return False
+    for item in list:
+        if not item.startswith(s):
+            return False
+    return True
+
+def is_ordered_list(list):
+    if len(list) == 0:
+        return False
+    line_no = 0    
+    for item in list:
+        line_no += 1
+        if not item.startswith(f"{line_no}. "):
+            return False
+    return True
+
+
+def block_to_block_type(block):
+
+    if len(block) == 0:
+        return BlockType.PARAGRAPH
+    
+    lines = block.split("\n")
+    line_count = len(lines)
+    
+    if line_count == 0:
+        return BlockType.PARAGRAPH
+    if lines[0].startswith("```") and lines[line_count-1].endswith("```"):
+        return BlockType.CODE
+    if list_items_all_start_with(lines, "# "):
+        return BlockType.HEADING1
+    if list_items_all_start_with(lines,"## "):
+        return BlockType.HEADING2
+    if list_items_all_start_with(lines,"### "):
+        return BlockType.HEADING3
+    if list_items_all_start_with(lines,"#### "):
+        return BlockType.HEADING4
+    if list_items_all_start_with(lines,"##### "):
+        return BlockType.HEADING5
+    if list_items_all_start_with(lines,"###### "):
+        return BlockType.HEADING6
+    if list_items_all_start_with(lines, ">"):
+        return BlockType.QUOTE
+    if list_items_all_start_with(lines, "- "):
+        return BlockType.UNORDERED_LIST
+    if list_items_all_start_with(lines, "* "):
+        return BlockType.UNORDERED_LIST
+    if is_ordered_list(lines):
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+    
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -76,6 +76,36 @@ This is the same paragraph on a new line
         )
         
         
+class TestBlockToBlockType(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(b.block_to_block_type("a"), b.BlockType.PARAGRAPH)
+        
+    def test_headings(self):
+        self.assertEqual(b.block_to_block_type('# aa\n# ff'), b.BlockType.HEADING1)
+        self.assertEqual(b.block_to_block_type('## aa'), b.BlockType.HEADING2)
+        self.assertEqual(b.block_to_block_type('### ss'), b.BlockType.HEADING3)
+        self.assertEqual(b.block_to_block_type('#### aa'), b.BlockType.HEADING4)
+        self.assertEqual(b.block_to_block_type('##### aa'), b.BlockType.HEADING5)
+        self.assertEqual(b.block_to_block_type('###### aa'), b.BlockType.HEADING6)
+        self.assertEqual(b.block_to_block_type('######aa'), b.BlockType.PARAGRAPH)
+
+    def test_code(self):
+        self.assertEqual(b.block_to_block_type('``` aa\n ff\n hhh\nbbb```'), b.BlockType.CODE)
+        self.assertEqual(b.block_to_block_type('``` aa\n ff\n hhh\n```'), b.BlockType.CODE)
+        self.assertEqual(b.block_to_block_type('``` \n ff\n hhh\n```'), b.BlockType.CODE)
+
+    def test_quote(self):
+        self.assertEqual(b.block_to_block_type('> aa\n> ff\n> hhh\n>bbb\n>```'), b.BlockType.QUOTE)
+        self.assertEqual(b.block_to_block_type('> aa\n> ff\n> hhh\n>bbb\n```'), b.BlockType.PARAGRAPH)
+
+    def test_unordered_list(self):
+        self.assertEqual(b.block_to_block_type('* item1\n* item2\n* item3'), b.BlockType.UNORDERED_LIST)
+        self.assertEqual(b.block_to_block_type('* item1\nitem2\n* item3'), b.BlockType.PARAGRAPH)
+        
+    def test_ordered_list(self):
+        self.assertEqual(b.block_to_block_type('1. item1\n2. item2\n3. item3'), b.BlockType.ORDERED_LIST)
+        self.assertEqual(b.block_to_block_type('1. item1\n4. item2\n3. item3'), b.BlockType.PARAGRAPH)
+        self.assertEqual(b.block_to_block_type('1. item1'), b.BlockType.ORDERED_LIST)
         
 if __name__ == "__main__":
     unittest.main()
